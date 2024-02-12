dd2fedfe552a4c01dcf995bd631da60635c110ef
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-02-12 17:34:45 +0000

    Prepare to ask for help with censored private methods in StaticPagesController and comments in the controller and index view.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,22 @@
 class StaticPagesController < ApplicationController
   def index
+    require 'flickr'
+    flickr = Flickr.new(api_key, shared_secret)
+    # This does not throw an error when I give it the values correctly,
+    # however I have not been able to get Figaro working correctly.
+    if params[:id]
+      flickr.people.getPublicPhotos(user_id: :id)
+    end
+  end
+
+  private
+
+  # Just coding it like this until I can get environment variables working in Figaro
+  def api_key
+    'censored_for_this_commit'
+  end
+
+  def shared_secret
+    'censored_for_this_commit'
   end
 end
